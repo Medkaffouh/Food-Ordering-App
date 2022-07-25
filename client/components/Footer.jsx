4b9321042b587a95d4b3a2030605c43df19d5332
Medkaffouh <kaffouhmohamed@gmail.com>
2022-07-25 23:37:26 +0100

    create footer component

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -1,7 +1,53 @@
-
+import styles from "../styles/Footer.module.css"
+import Image from "next/image"
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <Image src="/img/bg.png" layout="fill" alt="" objectFit="cover"/>
+      </div>
+      <div className={styles.item}>
+        <div className={styles.card}>
+          <h2 className={styles.motto}>
+            OH YES, WE DID. THE PIZZA, WELL BAKED SLICE OF PIZZA.
+          </h2>
+        </div>
+        <div className={styles.card}>
+          <h1 className={styles.title}>FIND OUR RESTAURANTS</h1>
+          <p className={styles.text}>
+            1723 R. Don Road #304.
+            <br /> Rabat, 323234
+            <br /> (602) 854-1920
+          </p>
+          <p className={styles.text}>
+            1723 R. Don Road #304.
+            <br /> Rabat, 323234
+            <br /> (602) 854-1920
+          </p>
+          <p className={styles.text}>
+            1723 R. Don Road #304.
+            <br /> Rabat, 323234
+            <br /> (602) 854-1920
+          </p>
+          <p className={styles.text}>
+            1723 R. Don Road #304.
+            <br /> Rabat, 323234
+            <br /> (602) 854-1920
+          </p>
+        </div>
+        <div className={styles.card}>
+          <h1 className={styles.title}>WORKING HOURS</h1>
+          <p className={styles.text}>
+            MONDAY UNTIL FRIDAY
+            <br /> 9:00 -22:00
+          </p>
+          <p className={styles.text}>
+            SATURDAY UNTIL SUNDAY
+            <br /> 12:00 -24:00
+          </p>
+        </div>
+      </div>
+    </div>
   )
 }
 
